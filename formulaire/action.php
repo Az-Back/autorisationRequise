ca37e8f7eaa7a0c9cdf17d942dd6ee0babbd74a5
Leroy Bryan <91132339+Az-Back@users.noreply.github.com>
2022-01-13 13:03:01 +0100

    Add files via upload

---
+++ formulaire/action.php
@@ -0,0 +1,32 @@
+<?php
+
+// Exo 1
+if(isset($_POST['age']) && isset($_POST['nom']))
+{
+
+    $nom = $_POST['nom'];
+    $age = $_POST['age'];
+    if ($age < 18) {
+        echo "<script>alert('Vous ne pouvez pas acceder au site')</script>";
+    } else {
+        echo "<script>alert('Vous pouvez accéder au site')</script>";
+    }
+}
+    
+//Exo2
+if(isset($_POST['bday']))
+{
+
+    $bday = date("Y",strtotime($_POST["bday"]));
+    $today = date("Y");
+    $end = $today - $bday;
+    
+    if ($end < 18) {
+        echo "<script>alert('Vous ne pouvez pas acceder au site')</script>";
+    } else {
+        echo "<script>alert('Vous pouvez accéder au site')</script>";
+    }
+}
+    
+
+
